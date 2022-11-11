ebbd685db81d22d10be22b392e8e5a731c6a6c99
Ivan Drahoner <idrahoner@gmail.com>
2022-11-11 14:49:38 +0200

    Worked 1st tast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import Profile from './profile/Profile';
+import user from '../data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Profile
+          name={user.username}
+          tag={user.tag}
+          avatar={user.avatar}
+          location={user.location}
+          followers={user.stats.followers}
+          likes={user.stats.likes}
+          views={user.stats.views}
+        />
+        React homework template
+      </div>
+    </>
   );
 };
