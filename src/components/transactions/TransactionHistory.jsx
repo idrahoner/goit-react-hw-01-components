a883ac8399a19329508e54ae1f3d4c97dd22d7f0
Ivan Drahoner <idrahoner@gmail.com>
2022-12-02 13:09:10 +0200

    Complete 4th task

--- src/components/transactions/TransactionHistory.jsx
+++ src/components/transactions/TransactionHistory.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import TransactionItem from './TransactionItem';
+import css from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ list }) {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
+    <table className={css.transactionHistory}>
+      <thead className={css.transactionHead}>
+        <tr className={css.headRow}>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
--- src/components/transactions/TransactionItem.jsx
+++ src/components/transactions/TransactionItem.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import css from './TransactionItem.module.css';
 
 export default function TransactionItem({ type, amount, currency }) {
   return (
-    <tr>
-      <td>{type}</td>
+    <tr className={css.bodyRow}>
+      <td className={css.type}>{type}</td>
       <td>{amount}</td>
       <td>{currency}</td>
     </tr>
